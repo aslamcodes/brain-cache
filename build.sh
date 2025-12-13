790d71c0775b69c736ff5d16616bc71814e3d778
aslamcodes <mohamedaslamcodes@gmail.com>
2025-12-13 19:39:26 +0530

    chore: update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-rm -rf quartz
+rm -rf quartz public
 git clone https://github.com/jackyzha0/quartz.git quartz
 
 rm -rf quartz/content/*
@@ -13,3 +13,6 @@ cp quartz.config.ts quartz/
 cd quartz
 npm ci
 npx quartz build
+cd ..
+
+mv quartz/public ./public
