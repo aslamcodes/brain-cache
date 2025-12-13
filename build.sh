973523255dc3443ce630a9bbd54eec2810529ef6
aslamcodes <mohamedaslamcodes@gmail.com>
2025-12-13 20:36:11 +0530

    update quartz config

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ git clone https://github.com/jackyzha0/quartz.git quartz
 rm -rf quartz/content/*
 cp -R aws computing development devops networking tools whitepapers attachments quartz/content/
 
+# home page
 cat > quartz/content/index.md <<'EOF'
 ---
 title: aslamnotes
@@ -15,13 +16,18 @@ title: aslamnotes
 # aslamnotes
 EOF
 
-# Quartz v4 config (correct)
+# Quartz v4 config (working)
 cat > quartz/quartz.config.ts <<'EOF'
-import { QuartzConfig } from "./quartz/config"
+import { QuartzConfig } from "./quartz.config"
 
 const config: QuartzConfig = {
-  site: {
-    title: "aslamnotes",
+  configuration: {
+    pageTitle: "aslamnotes",
+  },
+  plugins: {
+    transformers: [],
+    filters: [],
+    emitters: [],
   },
 }
 
