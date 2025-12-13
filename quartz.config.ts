d62a35dcf9d57e797f83512cbf5ee344669b5e96
aslamcodes <mohamedaslamcodes@gmail.com>
2025-12-13 19:06:16 +0530

    fix: update quartz content

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,3 +1,14 @@
-export default {
-  siteTitle: "aslamnotes",
+import { QuartzConfig } from "./quartz.config"
+
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "aslamnotes",
+  },
+  plugins: {
+    transformers: [],
+    filters: [],
+    emitters: [],
+  },
 }
+
+export default config
