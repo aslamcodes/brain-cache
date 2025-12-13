ff46a86a333b11003151927cb03639c4160385f5
aslamcodes <mohamedaslamcodes@gmail.com>
2025-12-13 16:05:53 +0530

    feat: add build command

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+git clone https://github.com/jackyzha0/quartz.git quartz
+rm -rf quartz/content/*
+
+rsync -av \
+  attachments aws cloud-security computing development devops networking templates tools whitepapers \
+  quartz/content/
+
+cd quartz
+npm ci
+npx quartz build
